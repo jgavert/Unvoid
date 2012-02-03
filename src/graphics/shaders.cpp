ec1a2c73b461d37236718e1622f96cfacf1efe3e
Juho Gävert <jgavert@cs.helsinki.fi>
2012-02-03 17:07:19 +0200

    did shader loading from file working linux

--- src/graphics/shaders.cpp
+++ src/graphics/shaders.cpp
@@ -1,36 +1,142 @@
 #include <string>
 #include <cstdlib>
-#include <sstream>
+#include <fstream>
+#include <iostream>
+#ifdef _WIN32
+#include <SDL.h>
+#include <GL\glew.h>
+#else
+#include "sdl/SDL.h"
+#include "glew/glew.h"
+#endif
 #include "shaders.h"
 
 
-shaders::shaders(void)
+Shaders::Shaders(void)
 {
 }
 
 
-shaders::~shaders(void)
+Shaders::~Shaders(void)
 {
 }
 
-string readShaderFile(string FileName)
+std::string Shaders::readShaderFile(std::string FileName)
 {
-string ShaderString = "";
-ifstream shaderFile;
-shaderFile.open(FileName);
-    while(!shaderFile.eof())
+  std::string ShaderString = "";
+  std::ifstream shaderFile;
+  shaderFile.open(FileName);
+  while(!shaderFile.eof())
+  {
+    std::string tempholder;
+    getline(shaderFile, tempholder);
+    ShaderString += tempholder;
+  }
+  shaderFile.close();
+  return ShaderString;
+}
+
+void Shaders::loadShaders(void)
+{
+  //debug for windows file management
+  std::ofstream myfile("example.txt");
+  if (myfile.is_open())
+  {
+    myfile << "This is a line." << std::endl;
+    myfile << "This is another line." << std::endl;
+    myfile.close();
+  }
+  //remove after clear
+  //local directory is the one where you run the main from
+  vertex = readShaderFile("shaders/simple.vertex");
+  fragment = readShaderFile("shaders/simple.fragment");
+}
+GLint Shaders::createShaders(void)
+{
+    const GLchar *VertexShader = vertex.c_str();
+    const GLchar *FragmentShader = fragment.c_str();
+    GLenum ErrorCheckValue = glGetError();
+
+    VertexShaderId = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(VertexShaderId, 1, &VertexShader, NULL);
+    glCompileShader(VertexShaderId);
+
+   //did this fail
+    int status;
+    glGetShaderiv(VertexShaderId,  GL_COMPILE_STATUS, &status);
+    int asdfg = 0;
+    glGetShaderiv(VertexShaderId, GL_INFO_LOG_LENGTH, &asdfg);
+    char* infoLog = (char*)malloc(sizeof(char)*1000);
+    glGetShaderInfoLog(VertexShaderId, asdfg, &asdfg, infoLog);
+      /*glGetShader(shaderID, GL_INFO_LOG_LENGTH, length); // length of log, needed for reading it back
+        glGetShaderInfoLog(shaderID, length, infoLog); // read back info log*/
+    FragmentShaderId = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(FragmentShaderId, 1, &FragmentShader, NULL);
+    glCompileShader(FragmentShaderId);
+
+    ProgramId = glCreateProgram();
+    ErrorCheckValue = glGetError();
+    if (ErrorCheckValue != GL_NO_ERROR)
     {
-        string tempholder;
-        getline(shaderFile, tempholder);
-        ShaderString.append(tempholder);
-        ShaderString.append("\n");
+        std::cerr << "ERROR1: Could not create the shaders: " << gluErrorString(ErrorCheckValue) << std::endl;
+        exit(-1);
     }
-shaderFile.close();
+    glAttachShader(ProgramId, VertexShaderId);
+    ErrorCheckValue = glGetError();
+    if (ErrorCheckValue != GL_NO_ERROR)
+    {
+        std::cerr << "ERROR2: Could not create the shaders: " << gluErrorString(ErrorCheckValue) << std::endl;
+        exit(-1);
+    }
+    glAttachShader(ProgramId, FragmentShaderId);
+    ErrorCheckValue = glGetError();
+    if (ErrorCheckValue != GL_NO_ERROR)
+    {
+        std::cerr << "ERROR3: Could not create the shaders: " << gluErrorString(ErrorCheckValue) << std::endl;
+        exit(-1);
+    }
+    glLinkProgram(ProgramId);
+    ErrorCheckValue = glGetError();
+    if (ErrorCheckValue != GL_NO_ERROR)
+    {
+        std::cerr << "ERROR4: Could not create the shaders: " << gluErrorString(ErrorCheckValue) << std::endl;
+        exit(-1);
+    }
+    //glUseProgram(ProgramId);
+    ErrorCheckValue = glGetError();
+    if (ErrorCheckValue != GL_NO_ERROR)
+    {
+        std::cerr << "ERROR5: Could not create the shaders: " << gluErrorString(ErrorCheckValue) << std::endl;
+        exit(-1);
+    }
 
-return ShaderString;
+    ErrorCheckValue = glGetError();
+    if (ErrorCheckValue != GL_NO_ERROR)
+    {
+        std::cerr << "ERROR6: Could not create the shaders: " << gluErrorString(ErrorCheckValue) << std::endl;
+        exit(-1);
+    }
+    return ProgramId;
 }
 
-shaders::loadShaders(void)
+void Shaders::destroyShaders(void)
 {
+    GLenum ErrorCheckValue = glGetError();
 
+    glUseProgram(0);
+
+    glDetachShader(ProgramId, VertexShaderId);
+    glDetachShader(ProgramId, FragmentShaderId);
+
+    glDeleteShader(FragmentShaderId);
+    glDeleteShader(VertexShaderId);
+
+    glDeleteProgram(ProgramId);
+
+    ErrorCheckValue = glGetError();
+    if (ErrorCheckValue != GL_NO_ERROR)
+    {
+    std::cerr << "ERROR: Could not destroy the shaders: " << gluErrorString(ErrorCheckValue) << std::endl;
+        exit(-1);
+    }
 }
